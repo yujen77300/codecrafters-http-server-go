8c39e1e90af624da52153a0786a26d43ed71c67b
Dylan Huang <yujenhuang24@gmail.com>
2025-05-06 22:42:28 +0800

    Respond with 200

--- app/main.go
+++ app/main.go
@@ -6,21 +6,42 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+type Server struct {
+	listener net.Listener
+}
 
 func main() {
+	s := Server{}
+	s.Start()
+}
 
+func (s *Server) Start() {
+	s.Listen()
+	defer s.Close()
+	conn := s.Accept()
+	fmt.Println("Accepted connection from: ", conn.RemoteAddr())
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
+
+func (s *Server) Listen() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	_, err = l.Accept()
+	s.listener = l
+}
+func (s *Server) Accept() net.Conn {
+	conn, err := s.listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	return conn
+}
+func (s *Server) Close() {
+	err := s.listener.Close()
+	if err != nil {
+		fmt.Println("Failed to close listener:", err.Error())
 	}
 }
